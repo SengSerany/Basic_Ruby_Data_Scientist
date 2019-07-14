89a29a28ce522bbdc20a813312f04a788e69e55b
SengSerany <cranyseng@gmail.com>
2019-07-14 23:08:39 +0200

    Ajout 00_journalists

---
+++ lib/00_journalists.rb
@@ -0,0 +1,170 @@
+def list
+	list = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+def all_list
+	all = list.length
+end
+
+def num_inlist
+nb_num_inlist = list.select { |num_inlist| num_inlist =~ /[0123456789]/}.size
+end
+
+def aude_inlist
+	aude_list = (list.grep (/.aude./)).size
+end
+
+def nb_beginmaj_inlist
+	nb_maj_inlist = list.select {|maj_inlist| maj_inlist =~ /^@[A..Z]/}.size
+end
+
+def nb_maj_inlist
+	nb_maj_inlist = list.select {|maj_inlist| maj_inlist =~ /.[A..Z]./}.size
+end
+
+def nb_underscore_list
+	nb_underscore_list = list.join.to_s.count "_"
+end
+
+def sort_list
+	list_sort = list.sort
+end
+
+def min50_list
+	min_list = list.sort_by {|minvalue| minvalue.length}
+	minimum_list = min_list [0..49]
+end
+
+def find_position
+	puts "\nTu veux trouver la position de quelqu'un dans l'array?
+Tape son handle si dessous:"
+	print "> "
+	handle = gets.chomp.to_s
+		if handle.include? "@"
+			position = list.index("#{handle}")
+			if position == nil
+				puts "\n
+La personne n'est pas dans la liste.
+\n"
+				find_position
+			else
+				puts "\n
+La personne est en position #{position}.
+\n"
+			end
+		else
+			puts "\n
+Pensé à tapper @ avant le handle de la personne.
+\n"
+			find_position
+		end
+end
+
+def boucleorend
+	puts "\nVeux-tu en rester la?
+Appuye Y pour quitter / Appuye N pour continuer.
+\n"
+	print "> "
+	@choice = gets.chomp.to_s
+
+	while @choice != "Y" && @choice !="N"
+
+		puts "\n
+Je n'ai pas compris,
+Appuye Y pour quitter / Appuye N pour continuer"
+
+		print "> "
+		@choice = gets.chomp
+	end
+
+		if @choice == "N"
+			menu
+
+		elsif @choice == "Y"
+			
+			puts "A la prochaine!"
+		end
+end
+
+def menu 
+	puts "
+-----------------------------------------------------------------------------
+\n
+	Bonjour, tu es dans un array de handle de journalites sur Twitter.
+Que souhaite tu faires?
+\n
+-----------------------------------------------------------------------------
+\n
+Tape 1 pour voir la liste entière des handles.
+Tape 2 pour savoir combiens il y a de journaliste dans l'array.
+Tape 3 pour savoir combien d'handle contiennent un numéro.
+Tape 4 pour savoir combien d'handle contiennent les 4 lettres du prénom Aude.
+Tape 5 pour savoir combien possède de majuscule à la première lettre aprés @.
+Tape 6 pour savoir combien il y a d'underscore au total.
+Tape 7 pour trier l'array par ordre alphabétique.
+Tape 8 pour savoir les 50 handle les plus courts.
+Tape 9 pour savoir la position d'un handle dans l'array"
+		print "> "
+		choise = gets.chomp.to_i
+
+	while (choise < 1 && choise > 10)
+		puts "Je n'ai pas compris ce que tu voulais faire :
+Tape 1 pour voir la liste entière des handles.
+Tape 2 pour savoir combiens il y a de journaliste dans l'array.
+Tape 3 pour savoir combien d'handle contiennent un numéro.
+Tape 4 pour savoir combien d'handle contiennent les 4 lettres du prénom Aude.
+Tape 5 pour savoir combien possède de majuscule à la première lettre aprés @.
+Tape 6 pour savoir combien il y a d'underscore au total.
+Tape 7 pour trier l'array par ordre alphabétique.
+Tape 8 pour savoir les 50 handle les plus courts.
+Tape 9 pour savoir la position d'un handle dans l'array"
+		print "> "
+		choise = gets.chomp
+	end
+
+	if choise == 1
+		puts "Voici la liste des journalistes de tout l'array:
+		\n
+	#{list}.
+	\n"
+
+	elsif choise == 2
+		puts "\n Il y a #{all_list} journalistes dans l'array.
+		\n"
+
+	elsif choise == 3
+		puts "\n Il y a #{num_inlist} handles qui contiennent un ou des numéro-s.
+		\n"
+
+	elsif choise == 4
+		puts "\n Il y a #{aude_inlist} handles qui contiennent les 4 lettres du prénom Aude à la suite.
+		\n"
+
+	elsif choise == 5
+		puts "\nIl y a #{nb_beginmaj_inlist} handles qui commencent avec une majuscule aprés @.
+		\n"
+
+	elsif choise == 6
+		puts "\nIl y a #{nb_underscore_list} underscore au total dans tout l'array.
+		\n"
+
+	elsif choise == 7
+		puts "\nVoici l'array trier par ordre alphabetique:
+#{sort_list}
+\n"
+
+	elsif choise == 8
+		puts "\nVoici la liste des 50 handles les plus court de l'array:
+#{min50_list}
+\n"
+
+	elsif choise == 9
+		find_position
+	end
+
+	boucleorend
+end
+
+
+
+menu
